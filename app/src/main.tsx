c0ac3173f376d67c7be62f14bebe55680ca4fafa
Daniel <daniel@blackhatmedia.com>
2026-03-10 15:19:50 -0500

    Test: Add App Version log to trigger update banner

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('App Version: 1.0.1');
+
 // Handle Safari Back-Forward Cache (bfcache)
 // If the page is restored from memory, trigger a hard reload to pick up the latest state
 window.addEventListener('pageshow', (event) => {
